bb241e76833a0d4aeb0809c57e1abf74e66ec54a
Jason <s947517134@163.com>
2021-03-09 12:08:51 +0800

    proxyBeanMethod

--- src/main/java/com/review/helloworld/HelloworldApplication.java
+++ src/main/java/com/review/helloworld/HelloworldApplication.java
@@ -1,15 +1,44 @@
 package com.review.helloworld;
 
+import com.review.helloworld.config.MyConfig;
+import com.review.helloworld.pojo.Pet;
+import com.review.helloworld.pojo.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 //本身就是Spring的一个组件
 //程序的主入口
+
+/**
+ * 主程序类
+ * @SpringBootApplication ： 这是一个springboot应用
+ */
 @SpringBootApplication
 public class HelloworldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+        //1.返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(HelloworldApplication.class, args);
+
+        //2.查看IOC容器中的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names){
+            System.out.println(name);
+        }
+
+        //3.从容器中获取组件
+        Pet tom1 = run.getBean("tom", Pet.class);
+        Pet tom2 = run.getBean("tom", Pet.class);
+        System.out.println("获取的两个组件是否是同一个组件:"+ (tom1==tom2));
+
+        MyConfig bean = run.getBean(MyConfig.class);
+        System.out.println(bean);
+
+        User user = bean.user01();
+        User user1 = bean.user01();
+        System.out.println(user == user1);
+
     }
 
 }
---
+++ src/main/java/com/review/helloworld/config/MyConfig.java
@@ -0,0 +1,33 @@
+package com.review.helloworld.config;
+
+import com.review.helloworld.pojo.Pet;
+import com.review.helloworld.pojo.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 1.配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
+ * 2.配置类本身也是组件
+ * 3.proxyBeanMethod: 代理bean的方法
+ *      Full(proxyBeanMethod = true) 【保证每个@Bean方法被调用多少次返回的组件都是单实例的】
+ *      Lite(proxyBeanMethod = false) 【每个@Bean方法被调用多少次返回的组件都是新创建的】
+ *      组件依赖必须使用FFull模式默认
+ */
+@Configuration //告诉SpringBoot这是一个配置类 == 配置文件
+public class MyConfig {
+
+    /**
+     * 外部无论对配置类中的这个组件注册方法调用多少次，获取的都是之前注册容器中的单实例对象
+     * @return
+     */
+    @Bean //给容器中添加组件，以方法名作为组件的id，返回类型就是组件类型，返回的值就是组件在容器中的实例
+    public User user01(){
+        return new User("jason",23);
+    }
+
+    @Bean("tom")
+    public Pet tomcatPet(){
+        return new Pet("tomcat");
+    }
+
+}
