29721a0b9715b17db6e3446cb776eef9959ed950
Jason <s947517134@163.com>
2021-03-18 20:59:26 +0800

    1、抽取公共页面 2、遍历数据

--- ManageEngine/src/main/java/com/boot/admin/controller/TableController.java
+++ ManageEngine/src/main/java/com/boot/admin/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.boot.admin.controller;
 
+import com.boot.admin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TableController {
@@ -14,7 +19,14 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model){
+        //表格内容的遍历
+        List<User> users = Arrays.asList(new User("zhangsan","123456"),
+                new User("jason","123"),
+                new User("admin","admin"));
+        model.addAttribute("users",users);
+
+
         return "table/dynamic_table";
     }
 
